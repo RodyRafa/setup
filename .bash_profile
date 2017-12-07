be8de2c0c8a999d4772a2346551e00cf775b5257
RodyRafa <rafael.rodrigues@netshoes.com>
2017-12-07 14:54:45 -0200

    [WIP] - Update Setup

--- .bash_profile
+++ .bash_profile
@@ -12,7 +12,7 @@ if [ "$GIT_HELPER" == "false" ]; then
   echo "Removes the GIT helpers"
   export PS1="\[\e[00;33m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[00;36m\][\w]\[\e[0m\] "
 else
-  export PS1="\[\e[00;33m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[00;36m\][\w]\[\e[0m\]\[\e[4;34m\]\$(parse_git_branch)\[\e[0m\]\$(git_status)"
+  export PS1="\[\e[00;33m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[00;36m\][\w]\[\e[0m\]\[\e[4;34m\]\$(parse_git_branch)\[\e[0;31m\]\$(git_status)\[\e[0m\] "
 fi
 
 
--- git_utils.sh
+++ git_utils.sh
@@ -6,31 +6,32 @@ function parse_git_branch() {
 #Get the status of the repo and chose a symbol accordingly
 # Symbols: ☺☻♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▀▐αßΓπΣσµΦΘΩδ∞φε∩≡±≥≤⌠⌡≈÷°∙√ⁿ²■ü
 function git_status() {
-		status=$(git status 2>&1 | tee)
-		flags=""
+    status=$(git status 2>&1 | tee)
+    flags=""
 
-		untracked=$(echo -n "${status}" 2> /dev/null | grep "Untracked files:" &> /dev/null; echo "$?")
-		if [[ ${untracked} == 0 ]]; then flags="${flags}?"; fi
+    untracked=$(echo -n "${status}" 2> /dev/null | grep "Untracked files:" &> /dev/null; echo "$?")
+    if [[ ${untracked} == 0 ]]; then flags="${flags}?"; fi
 
-		newfile=$(echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?")
-		if [[ ${newfile} == 0 ]]; then flags="${flags}+"; fi
+    newfile=$(echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?")
+    if [[ ${newfile} == 0 ]]; then flags="${flags}+"; fi
 
-		modified=$(echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?")
-		if [[ ${modified} == 0 ]]; then flags="${flags}±"; fi
+    modified=$(echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?")
+    if [[ ${modified} == 0 ]]; then flags="${flags}±"; fi
 
-		renamed=$(echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo $?)
-		if [[ ${renamed} == 0 ]]; then flags="${flags}→ "; fi
+    renamed=$(echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo $?)
+    if [[ ${renamed} == 0 ]]; then flags="${flags}→ "; fi
 
-		deleted=$(echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?")
-		if [[ ${deleted} == 0 ]]; then flags="${flags}x"; fi
+    deleted=$(echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?")
+    if [[ ${deleted} == 0 ]]; then flags="${flags}x"; fi
 
-		behind=$(echo -n  "${status}" 2> /dev/null | grep "Your branch is behind " &> /dev/null; echo "$?")
-		if [[ ${behind} == 0 ]]; then flags="${flags}↓"; fi
+    behind=$(echo -n  "${status}" 2> /dev/null | grep "Your branch is behind " &> /dev/null; echo "$?")
+    if [[ ${behind} == 0 ]]; then flags="${flags}↓"; fi
 
-		ahead=$(echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of " &> /dev/null; echo "$?")
-		if [[ ${ahead} == 0 ]]; then flags="${flags}↑"; fi
+    ahead=$(echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of " &> /dev/null; echo "$?")
+    if [[ ${ahead} == 0 ]]; then flags="${flags}↑"; fi
 
-		if [[ $flags != "" ]]; then
-			echo "[$flags]"
-		fi
+    if [[ $flags != "" ]]; then
+      echo -e "[$flags]"
+    fi
 }
+
